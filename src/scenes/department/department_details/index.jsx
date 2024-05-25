539eafd4b7bdc93594e22e8772726f22aabaac4e
Abhishek <abtargus7@gmail.com>
2024-05-25 12:58:17 +0530

    designation page created, department details added

---
+++ src/scenes/department/department_details/index.jsx
@@ -0,0 +1,16 @@
+import { useTheme } from "@mui/material"
+
+
+
+const DepartmentDetails = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
+    return (
+        <Box>
+            {/* <Header title={""} subtitle={"Add new team Member"} /> */}
+        </Box>
+    )
+}
+
+export default DepartmentDetails;
--- src/scenes/designation/index.jsx
+++ src/scenes/designation/index.jsx
@@ -1,95 +1,152 @@
-import { Button, Box, TextField, Typography, useTheme } from "@mui/material"
+import { Button, Box, TextField, Typography, useTheme } from "@mui/material";
 import { tokens } from "../../theme";
 import Header from "../../components/Header";
 import { Link } from "react-router-dom";
 import { Formik, Field } from "formik";
 import * as yup from "yup";
 // import { TextField } from "formik-mui";
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const AddDesignations = () => {
-    const theme = useTheme();
-    const colors = tokens(theme.palette.mode)
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [departmentsList, setDepartmentsList] = useState([]);
 
-    const handleFormSubmit = (values) => {
-        console.log(values)
-    }
+  const handleFormSubmit = async (values) => {
+    console.log(values);
+    await axios.post("/api/v1/designation/add", {
+      designationId: values.designationId,
+      designationName: values.designationName,
+      departments: values.departments
+    })
+    .then((response) => console.log(response))
+    .catch((error) => console.log(error))
+  };
 
+  const getDepartments = async () => {
+    const response = await fetch("/api/v1/employee/add/getDepartments");
+    const data = await response.json();
+    console.log(data.data);
+    setDepartmentsList(data.data);
+    // handleChange
+  };
 
+  useEffect(() => {
+    getDepartments();
+  }, []);
 
-    return (
-        <Box m="20px">
-            <Header title={"NEW DESIGNATION"} subtitle="Add new designation" />
+  // const onStateChange = async (event) => {
+  //   // console.log(event.target.value);
+  //   Formik.departments = event.target.value;
+  //   const result = await axios.get(
+  //     `/api/v1/employee/add/getDesignations/${event.target.value}`
+  //   );
+  //   // console.log(result);
+  //   setDesignationList(result.data);
+  // };
+  
 
-            <Formik
-                onSubmit={handleFormSubmit}
-                initialValues={initialValues}
-                validationSchema={checkoutSchema}
+  return (
+    <Box m="20px">
+      <Header title={"NEW DESIGNATION"} subtitle="Add new designation" />
+
+      <Formik
+        onSubmit={handleFormSubmit}
+        initialValues={initialValues}
+        validationSchema={checkoutSchema}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleBlur,
+          handleChange,
+          handleSubmit,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <Box
+              display="grid"
+              gap="30px"
+              gridTemplateColumns="repeat(4, minmax(0, 1fr))"
             >
-                {({
-                    values,
-                    errors,
-                    touched,
-                    handleBlur,
-                    handleChange,
-                    handleSubmit
-                }) => (
-                    <form onSubmit={handleSubmit}>
-                        <Box
-                            display="grid"
-                            gap="30px"
-                            gridTemplateColumns="repeat(4, minmax(0, 1fr))"
-                        >
-                            <Typography
-                                variant="h6"
-                                color={colors.gray[300]}
-                                sx={{m : "15px 0 5px 20px", gridColumn: "span 4"}}
-                            >
-                                Designation Data
-                            </Typography>
+              <Typography
+                variant="h6"
+                color={colors.gray[300]}
+                sx={{ m: "15px 0 5px 20px", gridColumn: "span 4" }}
+              >
+                Designation Data
+              </Typography>
 
-                            <TextField
-                                fullWidth
-                                variant="filled"
-                                type="text"
-                                label="Designation Id"
-                                onBlur={handleBlur}
-                                onChange={handleChange}
-                                value={values.designationId}
-                                name="designationId"
-                                error={!!touched.designationId && !!errors.designationId}
-                                sx={{ gridColumn: "span 2"}}
-                            />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Designation Id"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.designationId}
+                name="designationId"
+                error={!!touched.designationId && !!errors.designationId}
+                sx={{ gridColumn: "span 2" }}
+              />
 
-                            <TextField
-                                fullWidth
-                                variant="filled"
-                                type="text"
-                                label="Designation Name"
-                                onBlur={handleBlur}
-                                onChange={handleChange}
-                                value={values.designationName}
-                                name="designationName"
-                                error={!!touched.designationName && !!errors.designationName}
-                                sx={{ gridColumn: "span 2"}}
-                            />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Designation Name"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.designationName}
+                name="designationName"
+                error={!!touched.designationName && !!errors.designationName}
+                sx={{ gridColumn: "span 2" }}
+              />
 
-                            
-                            
-                        </Box>
-                    </form>
-                )}
-            </Formik>
-        </Box>
-    )
+              <label>Department</label>
+              <Field
+                // formControl={{ sx: sxFormControl }}
+                // component={Select}
+                // labelId="age-simple"
+                label="Department"
+                as="select"
+                name="departments"
+                onChange={handleChange}
+                // sx={{ backgroundColor: colors.gray[400] }}
+                // value={values.departmentName}
+              >
+                 {/* <MenuItem value={10}>Ten</MenuItem> */}
+                <option value="">Select</option>
+                {departmentsList.map((dept, index) => (
+                  <option key={index} value={dept._id}>
+                    {dept.departmentName}
+                  </option>
+                 
+                  
+                ))}
+              </Field>
+            </Box>
+            <Box display={"flex"} justifyContent={"end"} m={"20px 0 0 20px"}>
+              <Button type="submit" color="secondary" variant="contained">
+                Add Designation
+              </Button>
+            </Box>
+          </form>
+        )}
+      </Formik>
+    </Box>
+  );
 };
 
 const checkoutSchema = yup.object().shape({
-
+  designationId: yup.string().required("required"),
+  designationName: yup.string().required("required"),
 });
 
 const initialValues = {
-
-}
+  designationId: "",
+  designationName: "",
+};
 
 export default AddDesignations;
